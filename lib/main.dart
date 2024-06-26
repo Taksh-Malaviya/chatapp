566c85f347188a3c43e8e63664010f55fd16572c
taksh <taksh5834@gmail.com>
2024-06-26 17:43:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/myapp/myapp.dart';
+
+void main() {
+  runApp(
+    Whatsapp(),
+  );
+}
---
+++ lib/screen/view/homepage/listhome.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List whatsapp = [
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Het Gondaliya",
+    "sub": "flutter devlopar",
+    "time": "12 : 09",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Taksh Malaviya",
+    "sub": "flutter devlopar",
+    "time": "01 : 09",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Dhruv Kathiriya",
+    "sub": "Android devlopar",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Darshan",
+    "sub": "Busy ",
+    "time": "10 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Raj",
+    "sub": "Urgant call only",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Prince",
+    "sub": "Jay swaminarayan",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Vraj",
+    "sub": "At work",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Jash",
+    "sub": "Don't Disturb",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Alis",
+    "sub": "Hello",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Dharmik",
+    "sub": "How are  you",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Jay",
+    "sub": "Android devlopar",
+    "time": "11 : 23",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Ashish",
+    "sub": "Android devlopar",
+    "time": "11 : 23",
+  },
+];
+
+List whatsappstatus = [
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Het Gondaliya",
+    "sub": " 32 min ago",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Taksh Malaviya",
+    "sub": "1 hour ago",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Dhruv Kathiriya",
+    "sub": "2 hour ago",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Jaynesh Sir",
+    "sub": "2:12 hour ago",
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Dhruv Kathiriya",
+    "sub": " 4 hour ago",
+  },
+];
+
+List calls = [
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Dhruv ",
+    "sub": "6 , jun 12 : 36",
+    "icon2": Icon(
+      Icons.arrow_forward,
+      size: 20,
+      color: Colors.green,
+    ),
+    "icon": Icon(
+      Icons.phone_in_talk_outlined,
+      size: 28,
+      color: Colors.green,
+    ),
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Taksh Malaviya",
+    "sub": "7 , jun 1 : 22",
+    "icon2": Icon(
+      Icons.arrow_forward,
+      size: 20,
+      color: Colors.red,
+    ),
+    "icon": Icon(
+      Icons.phone_callback_outlined,
+      size: 28,
+      color: Colors.red,
+    ),
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Dhruv Kathiriya",
+    "sub": "10 , jun 1 : 01",
+    "icon2": Icon(
+      Icons.arrow_forward,
+      size: 20,
+      color: Colors.green,
+    ),
+    "icon": Icon(
+      Icons.missed_video_call_outlined,
+      size: 28,
+      color: Colors.green,
+    ),
+  },
+  {
+    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "name": "Jaynesh Sir",
+    "sub": "22 , jun 6 : 20",
+    "icon": Icon(
+      Icons.missed_video_call_outlined,
+      size: 28,
+      color: Colors.red,
+    ),
+    "icon2": Icon(
+      Icons.arrow_forward,
+      size: 20,
+      color: Colors.red,
+    ),
+  },
+];
